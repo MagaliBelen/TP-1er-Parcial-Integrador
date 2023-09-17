a9d6136f86314b13bf290374e70dbb34d720c3eb
Magali <maga.bjb@gmail.com>
2023-09-17 15:19:03 -0300

    cree clases alumno y profesor que heredan clase persona

---
+++ src/Alumno.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+public class Alumno extends Persona {
+    public Alumno(String nombre, String apellido, LocalDate fechaNacimiento, LocalDate fechaIngreso) {
+        super(nombre, apellido, fechaNacimiento, fechaIngreso);
+    }
+
+    
+}
---
+++ src/Profesor.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class Profesor extends Persona {
+    public Profesor(String nombre, String apellido, LocalDate fechaNacimiento, LocalDate fechaIngreso) {
+        super(nombre, apellido, fechaNacimiento, fechaIngreso);
+    }
+
+}
